5a7c608c761ad16f8a8a218367ea942dd9af91a1
naoki <2629936804@qq.com>
2023-08-01 13:33:15 +0800

    update: add nft script

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::fs::File;
+use std::io::{Read, Write};
 use std::net::SocketAddr;
+use std::path::Path;
 use std::str::FromStr;
 
 use aleo_rust::{
@@ -16,6 +19,8 @@ use tracing::Level;
 
 pub mod db;
 
+const RESULT_PATH: &str = "result.txt";
+
 #[derive(Debug, Parser)]
 #[clap(name = "auto-faucet")]
 pub struct Cli {
@@ -30,6 +35,12 @@ pub struct Cli {
 
     #[clap(long, default_value = "8989")]
     pub port: u16,
+
+    #[clap(long)]
+    pub path: Option<String>,
+
+    #[clap(long, default_value = RESULT_PATH)]
+    pub result_path: String,
 }
 
 #[tokio::main]
@@ -41,16 +52,62 @@ async fn main() {
         aleo_rpc,
         pk,
         from_height,
+        path,
+        result_path,
         port,
     } = cli;
 
     let pk = PrivateKey::<Testnet3>::from_str(&pk).expect("private key");
     let vk = ViewKey::try_from(&pk).expect("view key");
 
-    let faucet = AutoFaucet::new(aleo_rpc, pk, vk, from_height).expect("faucet");
+    let mut faucet = AutoFaucet::new(aleo_rpc, pk, vk, from_height).expect("faucet");
+    faucet.sync().expect("failed to sync aleo");
+
+    // TODO: add read script
+    if let Some(path) = path {
+        let mut result_file = File::options()
+            .create(true)
+            .append(true)
+            .open(result_path.clone())
+            .expect("result file");
+        let execs = get_exec_from_path(path, result_path);
+        for e in execs {
+            for _ in 0..3 {
+                match faucet.add_white_list(e.clone()) {
+                    Ok((nft_id, tx_id)) => {
+                        tracing::info!("Added nft: {:?} {:?}", nft_id, tx_id);
+                        result_file
+                            .write_all(format!("{} {}\n", nft_id, tx_id).as_bytes())
+                            .expect("write result");
+                        break;
+                    }
+                    Err(e) => {
+                        tracing::error!("Error adding nft: {:?}", e);
+                        tokio::time::sleep(std::time::Duration::from_secs(15)).await;
+                    }
+                }
+            }
+
+            if let Err(e) = faucet.sync() {
+                tracing::error!("Error syncing: {:?}", e);
+            }
+        }
+    }
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     faucet.sync_and_initial(addr).await.expect("server panic");
+}
+
+pub fn retry_with_times(
+    times: usize,
+    mut f: impl FnMut() -> anyhow::Result<()>,
+) -> anyhow::Result<()> {
+    for _ in 0..times {
+        if let Ok(_) = f() {
+            return Ok(());
+        }
+    }
+    Err(anyhow::anyhow!("retry failed"))
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -113,7 +170,7 @@ impl<N: Network> AutoFaucet<N> {
         let cur = self.network.get(&self.network_key)?.unwrap_or(0);
         let latest = self.client.latest_height()?;
         tracing::debug!("Requesting aleo blocks from {} to {}", cur, latest);
-        const BATCH_SIZE: usize = 500;
+        const BATCH_SIZE: usize = 50;
 
         for start in (cur..latest).step_by(BATCH_SIZE) {
             let end = (start + BATCH_SIZE as u32).min(latest);
@@ -135,6 +192,7 @@ impl<N: Network> AutoFaucet<N> {
         block.clone().into_serial_numbers().for_each(|sn| {
             let _ = self.unspent_records.remove(&sn.to_string());
         });
+
         // handle out
         for (commit, record) in block.clone().into_records() {
             if !record.is_owner(&self.vk) {
@@ -204,27 +262,6 @@ impl<N: Network> AutoFaucet<N> {
         blocks
     }
 
-    pub fn execute(&mut self, exec: Execution) -> anyhow::Result<String> {
-        let (rid, fee_record) = self.get_record()?;
-        let result = self.pm.execute_program(
-            exec.program_id,
-            exec.program_function,
-            exec.inputs.iter(),
-            exec.fee,
-            fee_record.clone(),
-            None,
-        );
-
-        if let Err(e) = &result {
-            if !e.to_string().contains("already exists in the ledger")
-                && !e.to_string().contains("Fee record does not have enough")
-            {
-                self.unspent_records.insert(&rid, &fee_record)?;
-            }
-        }
-        result
-    }
-
     fn initial(self) -> Self {
         let self_clone = self.clone();
         std::thread::spawn(move || loop {
@@ -274,6 +311,27 @@ impl<N: Network> AutoFaucet<N> {
         Ok((rid, record))
     }
 
+    pub fn execute(&mut self, exec: Execution) -> anyhow::Result<String> {
+        let (rid, fee_record) = self.get_record()?;
+        let result = self.pm.execute_program(
+            exec.program_id,
+            exec.program_function,
+            exec.inputs.iter(),
+            exec.fee,
+            fee_record.clone(),
+            None,
+        );
+
+        if let Err(e) = &result {
+            if !e.to_string().contains("already exists in the ledger")
+                && !e.to_string().contains("Fee record does not have enough")
+            {
+                self.unspent_records.insert(&rid, &fee_record)?;
+            }
+        }
+        result
+    }
+
     pub fn transfer(&mut self, addr: Address<N>, amount: u64) -> anyhow::Result<(String, String)> {
         tracing::warn!("transfering to {} amount {amount}", addr);
 
@@ -306,6 +364,70 @@ impl<N: Network> AutoFaucet<N> {
             }
         }
     }
+
+    pub fn add_nft(&mut self, nft_id: String) -> anyhow::Result<(String, String)> {
+        tracing::warn!("adding nft {nft_id}");
+
+        let nft_input = from_nft_id(nft_id.clone());
+
+        let (rid, fee_record) = self.get_record()?;
+
+        let inputs = vec![nft_input, "0scalar".to_string()];
+
+        let result = self.pm.execute_program(
+            "izar_testnet3_nft_v1.aleo",
+            "add_nft",
+            inputs.iter(),
+            40000,
+            fee_record.clone(),
+            None,
+        );
+
+        match result {
+            Ok(result) => Ok((nft_id, result)),
+            Err(e) => {
+                if !e.to_string().contains("already exists in the ledger")
+                    && (!e.to_string().contains("Fee record does not have enough")
+                        && fee_record.microcredits().unwrap() < 40000)
+                {
+                    tracing::warn!("reinsert unspent records");
+                    self.unspent_records.insert(&rid, &fee_record)?;
+                }
+                Err(e)
+            }
+        }
+    }
+
+    pub fn add_white_list(&mut self, addr: String) -> anyhow::Result<(String, String)> {
+        tracing::warn!("adding white list {addr}");
+
+        let (rid, fee_record) = self.get_record()?;
+
+        let inputs = vec![addr.to_string(), "1u8".to_string()];
+
+        let result = self.pm.execute_program(
+            "izar_testnet3_nft_v1.aleo",
+            "add_minter",
+            inputs.iter(),
+            40000,
+            fee_record.clone(),
+            None,
+        );
+
+        match result {
+            Ok(result) => Ok((addr.to_string(), result)),
+            Err(e) => {
+                if !e.to_string().contains("already exists in the ledger")
+                    && (!e.to_string().contains("Fee record does not have enough")
+                        && fee_record.microcredits().unwrap() < 40000)
+                {
+                    tracing::warn!("reinsert unspent records");
+                    self.unspent_records.insert(&rid, &fee_record)?;
+                }
+                Err(e)
+            }
+        }
+    }
 }
 
 async fn exec<N: Network>(
@@ -318,4 +440,35 @@ async fn exec<N: Network>(
     })?;
 
     Ok(Json(tid))
+}
+
+pub fn from_nft_id(nft_id: String) -> String {
+    let mut nft_bytes = [0u8; 32];
+    nft_bytes[0..nft_id.len()].copy_from_slice(nft_id.as_bytes());
+
+    let nft_1 = u128::from_le_bytes(nft_bytes[0..16].try_into().unwrap());
+    let nft_2 = u128::from_le_bytes(nft_bytes[16..32].try_into().unwrap());
+
+    format!("{{ data1:{nft_1}u128, data2:{nft_2}u128 }}")
+}
+
+pub fn get_exec_from_path(p: impl AsRef<Path>, filter_path: impl AsRef<Path>) -> Vec<String> {
+    let mut file = std::fs::File::open(p).expect("file");
+
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).expect("read");
+    let mut vec = vec![];
+    buf.lines().for_each(|e| {
+        vec.push(e.to_string());
+    });
+
+    // filter addr
+    {
+        if let Ok(mut file) = std::fs::File::open(filter_path) {
+            let mut buf = String::new();
+            file.read_to_string(&mut buf).expect("read");
+            vec.retain(|e| !buf.contains(e.to_string().as_str()));
+        }
+    }
+    vec
 }
